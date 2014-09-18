bf337fd3731f8363c3f784c0be248e8a808e193d
akosyako <akosyakozhabekova@gmail.com>
2014-09-18 15:48:30 +0600

    Laboratory work 1

---
+++ 11BD02103/LabOneNew/src/com/example/labonenew/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.labonenew;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	public final static String EXTRA_MESSAGE = "com.example.labonenew.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void sendMessage(View view){
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText)findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
+}
